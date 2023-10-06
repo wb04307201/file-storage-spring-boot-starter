b91ddd0c16cbb3907b7539ce7ecfe5de01be783b
wubo <wubo_aaa@163.com>
2023-10-06 18:28:19 +0800

    调整代码结构

--- src/main/java/cn/wubo/file/storage/servlet/FileStorageListServlet.java
+++ src/main/java/cn/wubo/file/storage/servlet/FileStorageListServlet.java
@@ -3,7 +3,6 @@ package cn.wubo.file.storage.servlet;
 import cn.wubo.file.storage.core.FileInfo;
 import cn.wubo.file.storage.core.FileStorageService;
 import cn.wubo.file.storage.exception.FileStorageRuntimeException;
-import cn.wubo.file.storage.record.IFileStroageRecord;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
@@ -28,29 +27,29 @@ public class FileStorageListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String contextPath = req.getContextPath();
-            Map<String, Object> data = new HashMap<>();
+        Map<String, Object> data = new HashMap<>();
 
-            FileInfo fileInfo = new FileInfo();
-            if (req.getMethod().equalsIgnoreCase("post")) {
-                Map<String, String[]> map = req.getParameterMap();
-                fileInfo.setPlatform(map.get("platform")[0]);
-                fileInfo.setAlias(map.get("alias")[0]);
-                fileInfo.setOriginalFilename(map.get("originalFilename")[0]);
-            }
+        FileInfo fileInfo = new FileInfo();
+        if (req.getMethod().equalsIgnoreCase("post")) {
+            Map<String, String[]> map = req.getParameterMap();
+            fileInfo.setPlatform(map.get("platform")[0]);
+            fileInfo.setAlias(map.get("alias")[0]);
+            fileInfo.setOriginalFilename(map.get("originalFilename")[0]);
+        }
 
-            data.put("list", fileStorageService.list(fileInfo));
-            data.put("contextPath", contextPath);
-            data.put("query", fileInfo);
+        data.put("list", fileStorageService.list(fileInfo));
+        data.put("contextPath", contextPath);
+        data.put("query", fileInfo);
 
-            freemarker.template.Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_23);
-            cfg.setClassForTemplateLoading(this.getClass(), "/template");
-            resp.setCharacterEncoding("UTF-8");
-            try {
-                Template template = cfg.getTemplate("storagelist.ftl", "UTF-8");
-                template.process(data, resp.getWriter());
-            } catch (TemplateException e) {
-                throw new FileStorageRuntimeException(e.getMessage(), e);
-            }
+        freemarker.template.Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_23);
+        cfg.setClassForTemplateLoading(this.getClass(), "/template");
+        resp.setCharacterEncoding("UTF-8");
+        try {
+            Template template = cfg.getTemplate("storagelist.ftl", "UTF-8");
+            template.process(data, resp.getWriter());
+        } catch (TemplateException e) {
+            throw new FileStorageRuntimeException(e.getMessage(), e);
+        }
     }
 
     @Override
